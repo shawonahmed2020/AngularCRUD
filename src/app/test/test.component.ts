3bd8234d8d11fc4c6b4c89c586c8da80e84028d5
shawonahmed2020 <shawonahmed.net@gmail.com>
2019-04-29 10:54:29 +0600

    bootstrapadded

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
   public name = 'Shawon Ahmed';
-
+  public isDisabled = true;
+  public successClass = 'text-success';
+  public hasError = true;
+  public greeting = '';
   constructor() { }
 
   ngOnInit() {
   }
 
+  onClick(event) {
+    console.log(event);
+    this.greeting = 'Welcome';
+  }
 }
